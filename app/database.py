84c0c9c2ee3816fb33a8dd52c93dd3157c228a81
mnavdev21 <miqonav@gmail.com>
2025-12-28 16:08:55 +0400

    fix DATABASE_URL to include psycopg2 driver

--- app/database.py
+++ app/database.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
-DATABASE_URL = "postgresql://pub_user:pub_pass@localhost/publications_db"
+# DATABASE_URL = "postgresql://pub_user:pub_pass@localhost/publications_db"
+DATABASE_URL = "postgresql+psycopg2://pub_user:pub_pass@localhost/publications_db"
+
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
